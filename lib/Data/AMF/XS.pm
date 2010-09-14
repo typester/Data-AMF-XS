0338399c9730ea1e973c60bdc6817f81de70bdb2
Daisuke Murase <typester@cpan.org>
2010-09-14 20:10:23 +0900

    Checking in changes prior to tagging of version 0.01_01.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 8455aaa..d1b14a8 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,4 +1,4 @@
     Revision history for Perl extension Data::AMF::XS
    
    -  0.01  Wed Sep  8 13:15:46 2010
    -        - original version
    +0.01_01 2010-09-14T19:41:11+09:00
    +        - first dev release

--- lib/Data/AMF/XS.pm
+++ lib/Data/AMF/XS.pm
@@ -3,7 +3,7 @@ use strict;
 use XSLoader;
 use base 'Exporter';
 
-our $VERSION = '0.01';
+our $VERSION = '0.01_01';
 
 our @EXPORT = qw/encode_amf0 decode_amf0/;
 
@@ -13,9 +13,11 @@ XSLoader::load __PACKAGE__, $VERSION;
 
 __END__
 
+=for stopwords amf amf0 amf3 deserialize RPC
+
 =head1 NAME
 
-Data::AMF::XS - Module abstract (<= 44 characters) goes here
+Data::AMF::XS - serialize / deserialize AMF data
 
 =head1 SYNOPSIS
 
@@ -26,11 +28,43 @@ Data::AMF::XS - Module abstract (<= 44 characters) goes here
 
 =head1 DESCRIPTION
 
-Stub documentation for this module was created by ExtUtils::ModuleMaker.
-It looks like the author of the extension was negligent enough
-to leave the stub unedited.
+This module provides simple and fast serialize/deserialize functions for AMF data.
 
-Blah blah blah.
+=head1 ALPHA LIMITATION
+
+This module is *ALPHA* release and has several limitations at this time.
+
+=over 4
+
+=item Support only AMF0 (and limited type)
+
+=item No AMF RPC Packet support (only AMF data)
+
+=back
+
+=head1 EXPORTED FUNCTIONS
+
+=head2 encode_amf0($obj, ...)
+
+   my $amf0_data = encode_amf0(@objs);
+
+Serialize perl objects to AMF data.
+
+=head2 decode_amf0($amf0_data) 
+
+   my @objs = decode_amf0($amf0_data);
+
+Deserialize AMF data to perl objects.
+
+NOTICE: this function may return list value when C<$amf0_data> contains multiple objects.
+So you should treat return value as list.
+If you need only first objects, you should do:
+
+    my ($obj) = decode_amf0($amf0_data);
+
+should not do:
+
+    my $obj = decode_amf0($amf0_data); # WRONG
 
 =head1 AUTHOR
 
@@ -38,7 +72,7 @@ Daisuke Murase <typester@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2009 by KAYAC Inc.
+Copyright (c) 2010 by KAYAC Inc.
 
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
